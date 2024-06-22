634c3dca7abd79a0c66dcd10e31bcfdaa1b8be77
mahuan-git <82866404+mahuan-git@users.noreply.github.com>
2024-06-22 21:20:43 +0800

    Delete NeuralNetworkQuantumState/local_energy directory

--- NeuralNetworkQuantumState/local_energy/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Usage: source env.sh
-
-cur_path=`pwd`
-export PYTHONPATH=$cur_path:$PYTHONPATH
-# export JULIA_LOAD_PATH=$cur_path/:$JULIA_LOAD_PATH
--- NeuralNetworkQuantumState/local_energy/include/switch_backend.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#ifdef BACKEND_CPU
-#include "calculate_local_energy_cpu.h"
-#elif defined(BACKEND_GPU)
-#include "calculate_local_energy.cuh"
-#endif
-
